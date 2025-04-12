0585ebac377e9a2390fcbb2727ad75c1701867d4
Govind <govindsys1008@gmail.com>
2025-04-12 05:42:15 +0530

    added merchandise page

--- esoc_25/brr/urls.py
+++ esoc_25/brr/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,page3
 urlpatterns = [
     path('', home, name='home'),
+    path('page3/', page3, name='page3'),
+
 ]
--- esoc_25/brr/views.py
+++ esoc_25/brr/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+def page3(request):
+    return render(request, 'page3.html')
 
